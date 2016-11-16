b90fdc50bb60a7f208ab356e8a3fb8f0c3df4f5b
Perez3110 <b.perez0113@gmail.com>
2016-11-16 10:35:18 -0600

    Add files via upload

---
+++ Conjuration.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Conjuration {
+
+	/**I want my (myself now) to work on switch statements and 
+	 * use a boolean and a character. These are things that were rarely encountered 
+	 * and could use some refreshment on these. The way this can be done is by
+	 *  counting the number of lightning strikes and stating how frequent the 
+	 * strikes were with an adjective. This can be incorporated into a switch statement,
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+		Scanner input= new Scanner(System.in);
+	
+	
+}
