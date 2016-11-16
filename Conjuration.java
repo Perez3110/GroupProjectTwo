95b8ca7a950cf8266064eb144184d09cf0e9d151
Perez3110 <b.perez0113@gmail.com>
2016-11-16 10:49:54 -0600

    Update Conjuration.java

--- Conjuration.java
+++ Conjuration.java
@@ -1,12 +1,18 @@
 import java.util.Scanner;
 public class Conjuration {
 
-	/**I want my (myself now) to work on switch statements and 
+	/**I want to work on switch statements and 
 	 * use a boolean and a character. These are things that were rarely encountered 
 	 * and could use some refreshment on these. The way this can be done is by
 	 *  counting the number of lightning strikes and stating how frequent the 
 	 * strikes were with an adjective. This can be incorporated into a switch statement,
-	 * 
+	 *but FIRST OF ALL, the severity of the storm must be chosen on a scale of 1-15,
+	 *which will be done using an integer. Then, based on severity, forms of precipitation will arise,
+	 * increasing severity will allow new products of the storm to be thrown into the fray, which will be included in
+	 *a two-dimensional array(One dimension will generate precipitation, while the second will generate its strength)
+	 * Then the user will be prompted to increase the severity of the storm. Should the user show mercy on the land, he or she
+	 * may enter 0 to put an end to the impact of the chosen storm. Or enter 1-5 as an increment to the level of severity.
+	 * This will be done with a one-dimensional array, which will cause the main part, the natural disaster. 
 	 * 
 	 * 
 	 * 
